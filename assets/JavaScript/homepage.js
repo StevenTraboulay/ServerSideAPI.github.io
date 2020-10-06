d1232be554c085999645cf14165c2656fa5ded62
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-10-06 17:26:35 -0400

    updated data retrieval

--- assets/JavaScript/homepage.js
+++ assets/JavaScript/homepage.js
@@ -1,18 +1,33 @@
-var getUserRepos = function(StevenTraboulay) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/StevenTraboulay/repos";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-  // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
-  });
-};
+  };
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+// get value from input eleme
+var username = nameInputEl.value.trim();
 
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a valid GitHub username");
+}
+    console.log(event);
+  };
 //executing function
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
 
 
   
